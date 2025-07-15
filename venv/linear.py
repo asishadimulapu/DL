65dbc1daf50528ef5a5e2991c97dc18efba6c035
asishadimulapu <asishdandy87@gmail.com>
2025-07-15 14:29:45 +0530

    first commit

---
+++ venv/linear.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+X=np.linspace(1,10,100)
+Y=2*X+np.random.randn(100)
+print(X)
+print(Y)
+
