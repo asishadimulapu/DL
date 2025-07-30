5b7d86be98767747c06b38dd39377f8ea4b03a94
asishadimulapu <150647669+asishadimulapu@users.noreply.github.com>
2025-07-30 23:12:16 +0530

    Update mlp_cifar.py

--- venv/mlp_cifar.py
+++ venv/mlp_cifar.py
@@ -4,7 +4,7 @@ from keras.datasets import cifar10
 from keras.utils import to_categorical
 import matplotlib.pyplot as plt
 
-
+#dont write in record
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -22,21 +22,29 @@ y_test = to_categorical(y_test)
 
 model = Sequential()
 model.add(Flatten(input_shape=(32, 32, 3)))
+model.add(Dense(1024, activation="relu"))
+model.add(Dense(512, activation="relu"))
 model.add(Dense(256, activation="relu"))
 model.add(Dense(128, activation="relu"))
-
 model.add(Dense(10, activation="softmax"))
 
 # Compile
 
-model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics = ["accuracy"])
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics = ["accuracy"])
 
 # Train 
 
-history = model.fit(X_train, y_train, epochs=3, batch_size=64)
+history = model.fit(X_train, y_train, epochs=30, batch_size=64, validation_split=0.2)
 
-#  Evaluate
-model.evaluate(X_train, y_train)
+# Evaluate
 
-#visualization
-plt.plot(epochs,)
+test_accuracy, loss = model.evaluate(X_test, y_test)
+print(f"test_accuracy:{test_accuracy}\nLoss:{loss}")
+
+# Visualization
+
+plt.plot(history.history['accuracy'], color="blue", label="train_accuracy")
+plt.plot(history.history['val_accuracy'], color="red", label = "val_accuracy")
+plt.legend()
+plt.title("Epochs vs Accuracy")
+plt.show()
